360df0336d7bdb506b8c002631b4f74cd6d506d3
zimingttkx <2147514473@qq.com>
2025-07-05 15:34:53 +0800

    Add NetworkSecurityException class and implement data extraction and MongoDB push functionality

--- push_data.py
+++ push_data.py
@@ -1,27 +1,63 @@
-from pymongo import MongoClient
+import os  # 用于处理文件和环境变量
+import sys # 用于获取异常信息
+from networksecurity.logging import logger # 用于日志记录
+from networksecurity.exception import NetworkSecurityException # 用于自定义异常处理
+import certifi # 用于处理SSL或者STL证书
+import pandas as pd # 用于数据处理
+import numpy as np # 用于数值计算
+import pymongo # 用于连接MongoDB数据库
+import json # 用于处理JSON数据
+from dotenv import load_dotenv
 
-# 1. 将 URI 修改为本地数据库的地址
-#    The URI is changed to the address of the local database.
-uri = "mongodb://localhost:27017/"
+load_dotenv(dotenv_path=".env")
 
-# 2. 创建客户端实例
-#    Create a client instance. For local connections, the server_api parameter is usually not needed.
-client = MongoClient(uri)
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 
-# 3. 发送一个 "ping" 来确认连接成功
-#    Send a "ping" to confirm a successful connection.
-try:
-    client.admin.command('ping')
-    print("您已成功连接到本地 MongoDB！")
-    print("You successfully connected to your local MongoDB!")
-except Exception as e:
-    print(e)
 
-# 4. (可选) 查看您本地的数据库列表
-#    (Optional) List the databases on your local instance.
-print("\n您本地的数据库列表 (List of your local databases):")
-print(client.list_database_names())
+# 构建ETL管道
+class NetworkDataExtractor():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
 
-# 关闭连接
-# Close the connection
-client.close()
+    # 定义一个函数 将csv文件转化为json格式
+    def cv_to_json(self,file_path):
+        try:
+            # 首先读取数据集
+            data = pd.read_csv(file_path)
+            # 删除没用的索引
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    # 定义一个函数 将数据集推送到MongoDB数据库
+    def push_data_to_mongodb(self,records,database,collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[database]
+            self.collection = self.database[collection]
+
+            # 将数据集推送到MongoDB数据库
+            self.collection.insert_many(self.records)
+            return (len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+if __name__ == "__main__":
+    # 定义数据集路径
+    file_path = "Network_Data/phisingData.csv"
+    database = "data"
+    collection = "NetworkData"
+    networkobj = NetworkDataExtractor()
+    records = networkobj.cv_to_json(file_path)
+    print(f"转化后的json数据是: {records}")  # 打印前5条记录以验证转换是否成功
+    num_of_records = networkobj.push_data_to_mongodb(records,database,collection)
+    print(f"成功推送{num_of_records}条数据到MongoDB数据库")
