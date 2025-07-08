861ce67bbef4cafa7c45c420f5bb18571c60c35f
zimingttkx <2147514473@qq.com>
2025-07-08 21:15:31 +0800

    添加数据验证和数据转换的产出物类，更新配置以支持数据转换

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -48,3 +48,12 @@ class DataValidationConfig:
         self.drift_report_file_path: str = os.path.join(self.data_validation_dir,
                                                         training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
                                                         training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
+
+
+# 这个类用来定义数据转换的配置
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_TRANSFORMATION_DIR_NAME)
+        self.transformed_train_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DIR, training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"))
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DIR, training_pipeline.TEST_FILE_NAME.replace("csv", "npy"))
+        self.transformed_object_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR, training_pipeline.DATA_TRANSFORMATION_OBJECT_FILE_NAME)
